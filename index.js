59fc58a4b4d56a0dc757376ae12652eed3eaddee
Mohamed Sujahath <mohamed.sujahath@gmail.com>
2017-11-19 15:36:00 +0530

    Modified for user typing indicator

--- index.js
+++ index.js
@@ -157,14 +157,14 @@ var connectedUsers={};
 
     });
 
-    socket.on('userTyping', (conversation) => {
-      console.log('Typing event received' + conversation.typingUserEmail + "-" + conversation.typingUserName);
+    socket.on('userTyping', (typingConversation) => {
+      console.log('Typing event received' + typingConversation.typingUserEmail + "-" + typingConversation.typingUserName + "-" + typingConversation.receiverEmail);
       //io.sockets.in(conversation).emit('refresh messages', conversation);
       //console.log('Socket ID of receiver: ' + connectedUsers[conversation.recipientEmail]);
 
-      users.findOne({'email':conversation.typingUserEmail}, function(err, user) {
-          console.log("Socket ID of the Receiver: " + user.socketID + " - " + conversation.typingUserEmail);
-          socket.to(user.socketID).emit('showUserTyping', conversation);
+      users.findOne({'email':typingConversation.receiverEmail}, function(err, user) {
+          console.log("Socket ID of the Receiver: " + user.socketID + " - " + typingConversation.typingUserEmail);
+          socket.to(user.socketID).emit('showUserTyping', typingConversation);
       });
 
     });
